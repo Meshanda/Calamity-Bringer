9a0c0d8ed0460b0739ca09c1311d270a48316cd9
Killian <killian.helpy@gmail.com>
2023-05-24 20:15:53 +0200

    Pause Screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,16 +22,36 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private IntVariable score;
     [HideInInspector] public UnityEvent OnScoreUpdate;
+    
+    [Space(10)]
+    [Header("Canvas")]
+    [SerializeField] private GameObject _pauseCanvas;
+    [SerializeField] private GameObject _hudCanvas;
+    
+    [Space(10)]
     [SerializeField] private List<GameObject> _objectToDisableOnVictory;
+
+    public static event Action GameUnpaused;
 
     private void OnEnable()
     {
         TimerSystem.TimerFinished += OnTimerFinished;
+        PauseSystem.GamePaused += OnGamePaused;
     }
 
     private void OnDisable()
     {
         TimerSystem.TimerFinished -= OnTimerFinished;
+        PauseSystem.GamePaused -= OnGamePaused;
+    }
+
+    private void OnGamePaused()
+    {
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        
+        _pauseCanvas.SetActive(true);
+        _hudCanvas.SetActive(false);
     }
 
     private void Start()
@@ -39,6 +59,9 @@ public class GameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         score.value = 0;
         OnScoreUpdate.Invoke();
+
+        _hudCanvas.SetActive(true);
+        _pauseCanvas.SetActive(false);
     }
 
     public void GainPoints(int points)
@@ -53,5 +76,25 @@ public class GameManager : MonoBehaviour
         
         SceneManager.LoadSceneAsync(SceneReferences.END_SCENE, LoadSceneMode.Additive)
             .completed += _ => Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ClickUnpause()
+    {
+        UnPause();
+    }
+
+    private void UnPause()
+    {
+        GameUnpaused?.Invoke();
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        _pauseCanvas.SetActive(false);
+        _hudCanvas.SetActive(true);
+    }
+
+    public void ClickMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneReferences.MAIN_MENU);
     }
 }
