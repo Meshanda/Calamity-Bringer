e499ce0683b3add843bf91415265332274d86e16
Killian <killian.helpy@gmail.com>
2023-05-26 09:16:24 +0200

    Tweak game scene

--- Assets/Scripts/Building/BandageBuilding.cs
+++ Assets/Scripts/Building/BandageBuilding.cs
@@ -1,23 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BandageBuilding : MonoBehaviour
 {
-    [SerializeField] private int _points;
-    [SerializeField] private GameManager _gm; 
-    // Start is called before the first frame update
-    public void OnCollisionEnter(Collision collision)
-    {
-        _gm.GainPoints(_points);
-        Destroy(gameObject);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name + " a");
-        _gm.GainPoints(_points);
-        Debug.Log(this+" "+gameObject.name);
+        GameManager.Instance.OnBuildingDestroyed(gameObject);
         Destroy(gameObject);
     }
 }
